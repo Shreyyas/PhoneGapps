a7ff931e0a7b197029b0bb9f3ab7340dbafa3ced
Shreyyas Vanarase <shreyyas@gatech.edu>
2016-01-19 17:17:08 -0800

    Basic App Implementation

---
+++ Basic-App/www/js/index.js
@@ -0,0 +1,7 @@
+//Listen for when device is ready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+// On Device Ready Listener
+function onDeviceReady() {
+    console.log('Device is ready');
+}
